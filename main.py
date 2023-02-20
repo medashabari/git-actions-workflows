5fd338440fc057159f717c37286f1bc9521a7cd7
medashabari <medashabari@gmail.com>
2023-02-20 20:11:53 +0530

    modified:   .github/workflows/build.yml 	modified:   main.py 	new file:   requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello, World')
+from flask import Flask , render_template , request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Succssfull congratulations"
+
+if __name__ == '__main__':
+    app.run()
